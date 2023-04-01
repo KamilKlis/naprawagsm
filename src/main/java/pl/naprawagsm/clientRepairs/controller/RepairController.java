f5846dc3cfd079510b26e9afa0ce9fa5cb758c9e
Kamil Klis <kamil.klis@o2.pl>
2023-04-01 20:46:54 +0200

    Added option to save Repair to Repository Changed repairController. After press to fill form at serviceLabel we save new Repair to Repository

--- src/main/java/pl/naprawagsm/clientRepairs/controller/RepairController.java
+++ src/main/java/pl/naprawagsm/clientRepairs/controller/RepairController.java
@@ -2,12 +2,26 @@ package pl.naprawagsm.clientRepairs.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.fasterxml.jackson.core.sym.Name;
+
+import jakarta.websocket.server.PathParam;
+import pl.naprawagsm.clientRepairs.model.RepairService;
 import pl.naprawagsm.clientRepairs.repository.Repair;
 
 @Controller
 public class RepairController {
+	
+	RepairService repairService;
+	
+	public RepairController(RepairService repairService) {
+		super();
+		this.repairService = repairService;
+	}
+
 	
 	@RequestMapping("/kwit")
 	public String serviceLabel() {
@@ -15,8 +29,11 @@ public class RepairController {
 	}
 
 	@RequestMapping("/twojeserwisy")
-	public String currentRepairs(Model model) {
-		model.addAttribute("repair", new Repair());
+	public String currentRepairs(Model model,@RequestParam String modeltelefonu, @RequestParam String opisusterki,
+								@RequestParam String stantelefonu, @RequestParam int maxkwotanaprawy,
+								@RequestParam String imięinazwisko, @RequestParam String numertelefonu) {
+		Repair repair =new Repair(modeltelefonu, stantelefonu, opisusterki, maxkwotanaprawy, imięinazwisko, numertelefonu);
+		repairService.addRepair(repair);
 		return "currentRepairs";
 	}
 	
