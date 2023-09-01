cd7015655a901866d7cabec0e19fa2e1bab8d246
KamilKlisLaptop <kamil.klis@o2.pl>
2023-09-01 23:57:04 +0200

    Added ExceptionHandler in RepairApi

--- src/main/java/pl/naprawagsm/api/RepairApi.java
+++ src/main/java/pl/naprawagsm/api/RepairApi.java
@@ -2,20 +2,28 @@ package pl.naprawagsm.api;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import jakarta.validation.Valid;
 import pl.naprawagsm.clientRepairs.model.RepairService;
 import pl.naprawagsm.clientRepairs.repository.dto.RepairDto;
 
@@ -43,12 +51,19 @@ public class RepairApi {
 	}
 	
 	@PostMapping()
-	public ResponseEntity<RepairDto> addRepair(@RequestBody RepairDto repair) {
+	public ResponseEntity<RepairDto> addRepair(@Valid @RequestBody RepairDto repair) {
 		repairService.addRepair(repair);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
 				.path("{/id}").buildAndExpand(repair.getId())
 				.toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public Map<String, String> handleValidationException(MethodArgumentNotValidException ex){
+		return ex.getBindingResult().getFieldErrors()
+			.stream().collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
 	}
 	
 	@PutMapping("/{id}")
