41b34b3f41885c2f4c8b7962ab14990d75ed95d5
Kamil Klis <kamil.klis@o2.pl>
2023-09-26 20:39:32 +0200

    Added css to login.html and register.html

--- src/main/java/pl/naprawagsm/clientRepairs/controller/RepairController.java
+++ src/main/java/pl/naprawagsm/clientRepairs/controller/RepairController.java
@@ -1,6 +1,5 @@
 package pl.naprawagsm.clientRepairs.controller;
 
-import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
