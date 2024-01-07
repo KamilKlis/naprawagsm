ac8ee03001b815d5041a40467cc36d77d1afa430
Kamil Klis <kamil.klis@o2.pl>
2024-01-08 00:00:15 +0100

    Fixed --restart unless-stopped in Dockerfile

--- build-and-restart.sh
+++ build-and-restart.sh
@@ -1,4 +1,4 @@
 docker build -t naprawagsm .
 docker stop naprawagsm || true
 docker rm naprawagsm || true
-docker run -d -p 8080:8080 --name=naprawagsm -e SPRING_PROFILES_ACTIVE=prod --network naprawagsm-network --restart unless:stopped naprawagsm
+docker run -d -p 8080:8080 --name=naprawagsm -e SPRING_PROFILES_ACTIVE=prod --network naprawagsm-network --restart unless-stopped naprawagsm
